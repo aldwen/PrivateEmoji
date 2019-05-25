7d795e85a915a043ba1de632809f9857678fcfaa
温 温 <7362677@qq.com>
2019-05-26 01:12:13 +0800

    编写models的代码

--- src/models.py
+++ src/models.py
@@ -1,8 +1,8 @@
 class EmojisymblewithInfo:
-'''
-用于存放emoji符号和附属信息的类
-'''
-    def __init__(self,symblechar,info中文名="",info英文描述="",info自定义短语=""):
+    '''
+    用于存放emoji符号和附属信息的类
+    '''
+    def __init__(self,symblechar,info中文名='Null',info英文描述='Null',info自定义短语='Null'):
         self.symblechar=symblechar
         self.info中文名=info中文名
         self.info英文描述=info英文描述
@@ -13,5 +13,9 @@ class EmojisymblewithInfo:
     def add中文名(self,data):
         self.name中文=data
 
+if __name__=="__main__":
+    emojixiaolian=EmojisymblewithInfo('😵')
+    print(emojixiaolian)
+    
 
 
