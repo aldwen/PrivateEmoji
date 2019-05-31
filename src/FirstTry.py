406f147f48f0d2e36f15f1970f19aa85b6dce4a9
温 温 <7362677@qq.com>
2019-05-31 20:57:19 +0800

    修改调试路径

--- src/FirstTry.py
+++ src/FirstTry.py
@@ -3,20 +3,22 @@
 import models
 import pickle
 
-changepath=2
+'''
+changepath=1
 if changepath==1:
     paths=r'..\\'
 elif changepath==2:
     paths=r'.\\PrivateEmoji\\'
+'''
 
 try:
-    pkl_file=open(paths+r'temp\mylist.pkl','rb')
+    pkl_file=open(r'..\temp\mylist.pkl','rb')
     a_emojiList=pickle.load(pkl_file)
     pkl_file.close()
     print('Load data form PKL file')
 except IOError:
     a_emojiList=models.emlist()
-    a_emojiList.LoadformXXL(paths+r"resources\xxl-emoji.json")
+    a_emojiList.LoadformXXL(r"..\resources\xxl-emoji.json")
     print("Load data from JSON file") 
 
 
@@ -29,10 +31,10 @@ a_emojiList.addsymble(bbb)
 for symble in a_emojiList.list:
     print(symble,end=' ')
 
-pkl_file=open(r'.\PrivateEmoji\temp\mylist.pkl','wb')
+pkl_file=open(r'..\temp\mylist.pkl','wb')
 pickle.dump(a_emojiList,pkl_file)
 pkl_file.close()
 
-a_emojiList.SavetoJson(r'.\PrivateEmoji\temp\myjson.json')
+a_emojiList.SavetoJson(r'..\temp\myjson.json')
 
-models.savetoSougou(r'.\PrivateEmoji\temp\sougou.txt',a_emojiList.list,Default_Position=2)
+models.savetoSougou(r'..\temp\sougou.txt',a_emojiList.list,Default_Position=2)
